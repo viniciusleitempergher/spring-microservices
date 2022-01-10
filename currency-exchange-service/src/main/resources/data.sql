b2520ed43b47a3dfdf526ee646e0e053de5bd59d
Vinícius Gabriel Leitempergher <viniciusgabriel.com@gmail.com>
2022-01-09 22:29:58 -0300

    class 17

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,48 @@
+insert into currency_exchange
+(
+   id,
+   currency_from,
+   currency_to,
+   conversion_multiple,
+   environment
+)
+values
+(
+   1001,
+   'USD',
+   'BRL',
+   5.64,
+   ''
+);
+insert into currency_exchange
+(
+   id,
+   currency_from,
+   currency_to,
+   conversion_multiple,
+   environment
+)
+values
+(
+   1002,
+   'EUR',
+   'BRL',
+   6.4,
+   ''
+);
+insert into currency_exchange
+(
+   id,
+   currency_from,
+   currency_to,
+   conversion_multiple,
+   environment
+)
+values
+(
+   1003,
+   'AUD',
+   'BRL',
+   4.04,
+   ''
+);
---
+++ limits-service/src/main/java/com/viniciusleitempergher/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,22 @@
+package com.viniciusleitempergher.microservices.limitsservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.viniciusleitempergher.microservices.limitsservice.bean.Limits;
+import com.viniciusleitempergher.microservices.limitsservice.configuration.Configuration;
+
+@RestController
+public class LimitsController {
+
+	@Autowired
+	private Configuration configuration;
+
+	@GetMapping("/limits")
+	public Limits retrieveLimits() {
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
+//		return new Limits(1, 1000);
+	}
+
+}
