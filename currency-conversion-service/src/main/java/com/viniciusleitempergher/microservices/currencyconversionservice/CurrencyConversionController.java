60c7304300c63ffa01fffa888eccdf4a1c2e40a3
Vinícius Gabriel Leitempergher <viniciusgabriel.com@gmail.com>
2022-01-09 23:22:30 -0300

    class 18 - using feign REST Client for service invocation

--- currency-conversion-service/src/main/java/com/viniciusleitempergher/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/viniciusleitempergher/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -2,6 +2,7 @@ package com.viniciusleitempergher.microservices.currencyconversionservice;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyConversionController {
 
-	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
+	@Autowired
+	private CurrencyExchangeProxy proxy;
+
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
-		return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+
+		currencyConversion.setQuantity(quantity);
+		currencyConversion.setTotalCalculatedAmount(quantity.multiply(currencyConversion.getConversionMultiple()));
+		currencyConversion.setEnvironment(currencyConversion.getEnvironment() + " feign");
+
+		return currencyConversion;
 	}
 
 }
