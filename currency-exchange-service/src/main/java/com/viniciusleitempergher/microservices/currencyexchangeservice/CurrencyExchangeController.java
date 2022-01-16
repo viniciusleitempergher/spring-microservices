bfb4a2315a535d8f62848556421a3d245bb9eaf9
Vinícius Gabriel Leitempergher <viniciusgabriel.com@gmail.com>
2022-01-15 23:56:26 -0300

    connecting microservices and gateway with zipkin

--- currency-exchange-service/src/main/java/com/viniciusleitempergher/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/viniciusleitempergher/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.viniciusleitempergher.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+
+	Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
 	@Autowired
 	private CurrencyExchangeRepository repository;
@@ -20,12 +24,14 @@ public class CurrencyExchangeController {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 
+		logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 
 		if (currencyExchange == null) {
 			throw new RuntimeException("Unable to find data for " + from + " to " + to);
 		}
-		
+
 		String port = environment.getProperty("local.server.port");
 		currencyExchange.setEnvironment(port);
 
